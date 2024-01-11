b74d59f0c7097cf214ee45da6a840965d3840322
unknown <nurhidayat.adi01@gmail.com>
2024-01-11 21:56:11 +0700

    edit project section and deleting some file

--- src/component/ProjectCard.jsx
+++ src/component/ProjectCard.jsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
-    <div>
+    <div className="bg-[#161617] p-3 rounded-xl">
       <div
         className="h-52 md:h-72 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover", backgroundPosition: "center" }}
+        style={{ background: `url(${imgUrl})`, backgroundSize: "cover", backgroundPosition: "center"}}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
@@ -18,7 +18,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           </Link>
         </div>
       </div>
-      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
+      <div className="text-white rounded-b-xl mt-3 py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
